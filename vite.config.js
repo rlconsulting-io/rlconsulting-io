e1fbb36cce56d354b442d670e086e2057992c24c
Régis Tremblay Lefrançois <rtlefrancois@agri-marche.com>
2022-07-15 09:35:47 -0400

    removed base option

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/rlconsulting-io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
