1ab39302f8b2686aa3d0c8d7db6d917c45a1c275
Régis Tremblay Lefrançois <rtlefrancois@agri-marche.com>
2022-07-15 09:27:45 -0400

    fixed typoe in base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/rlsonculting-io/',
+  base: '/rlconsulting-io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
